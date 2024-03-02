d3ed40f64d6f17ce21695c13ad06957c82fbf974
Joao Vitor <joaovitorvilar8@gmail.com>
2024-03-01 21:33:19 -0300

    Criei o arquivo Index em 01/03/2024

---
+++ aula07/src/aula07/Aula07.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package aula07;
+
+/**
+ *
+ * @author Caio Cintra B. Paula <caioexecutivo11@gmail.com>
+ * @date $(date?date?string("dd/MM/yyyy")}
+ * @brief Class $(name)
+*/
+public class Aula07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ aula07/src/testeaula07/testegithub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package testeaula07;
+
+/**
+ *
+ * @author Caio Cintra B. Paula <caioexecutivo11@gmail.com>
+ * @date 01/03/2024
+ * @brief Class $(name)
+ */
+public class testegithub {
+
+}
